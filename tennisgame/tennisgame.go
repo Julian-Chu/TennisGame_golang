dd853943ae2069f8085f27c6978660f4d125f288
JulianChu <yulang.chu@gmail.com>
2019-01-19 23:19:38 +0100

    refactor

--- tennisgame/tennisgame.go
+++ tennisgame/tennisgame.go
@@ -17,23 +17,63 @@ var lookup = map[int]string{
 }
 
 func (game TennisGame) Score() string {
-	if game.firstPlayerScoreTimes != game.secondPlayerScoreTimes {
-		player := game.firstPlayerName
-		if game.firstPlayerScoreTimes < game.secondPlayerScoreTimes {
-			player = game.secondPlayerName
-		}
-		if game.firstPlayerScoreTimes > 3 || game.secondPlayerScoreTimes > 3 {
-			if math.Abs(float64(game.firstPlayerScoreTimes-game.secondPlayerScoreTimes)) == 1 {
-				return player + " Adv"
+	if game.isScoreDifferent() {
+		if game.IsReadyForWin() {
+			if game.IsAdv() {
+				return game.getAdvPlayer(game.getPlayer())
 			}
-			return player + " Win"
+			return game.getWinner(game.getPlayer())
 		}
-		return lookup[game.firstPlayerScoreTimes] + " " + lookup[game.secondPlayerScoreTimes]
+		return game.getNormalScore()
 	}
 
-	if game.firstPlayerScoreTimes >= 3 {
-		return "Deuce"
+	if game.IsDeuce() {
+		return game.getDeuce()
 	}
+	return game.getAll()
+}
+
+func (game TennisGame) isScoreDifferent() bool {
+	return game.firstPlayerScoreTimes != game.secondPlayerScoreTimes
+}
+
+func (game TennisGame) getPlayer() string {
+	player := game.firstPlayerName
+	if game.firstPlayerScoreTimes < game.secondPlayerScoreTimes {
+		player = game.secondPlayerName
+	}
+	return player
+}
+
+func (game TennisGame) IsReadyForWin() bool {
+	return game.firstPlayerScoreTimes > 3 || game.secondPlayerScoreTimes > 3
+}
+
+func (game TennisGame) IsAdv() bool {
+	return math.Abs(float64(game.firstPlayerScoreTimes-game.secondPlayerScoreTimes)) == 1
+}
+
+func (game TennisGame) getAdvPlayer(player string) string {
+	return player + " Adv"
+}
+
+func (game TennisGame) getWinner(player string) string {
+	return player + " Win"
+}
+
+func (game TennisGame) getNormalScore() string {
+	return lookup[game.firstPlayerScoreTimes] + " " + lookup[game.secondPlayerScoreTimes]
+}
+
+func (game TennisGame) IsDeuce() bool {
+	return game.firstPlayerScoreTimes >= 3
+}
+
+func (game TennisGame) getDeuce() string {
+	return "Deuce"
+}
+
+func (game TennisGame) getAll() string {
 	return lookup[game.firstPlayerScoreTimes] + " All"
 }
 
