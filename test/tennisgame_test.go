14b5b1a5a56c3e98d1e05d4347e3c4850f78cf6d
JulianChu <yulang.chu@gmail.com>
2019-01-22 21:58:00 +0100

    refactor

--- test/tennisgame_test.go
+++ test/tennisgame_test.go
@@ -7,6 +7,15 @@ import (
 )
 
 func Test_LoveAll(t *testing.T) {
+	g := NewGame()
+	ScoreShouldBe(t, "Love All", g)
+}
+
+func ScoreShouldBe(t *testing.T, expected string, g *tennisgame.TennisGame) bool {
+	return assert.Equal(t, expected, g.Score(), "")
+}
+
+func NewGame() *tennisgame.TennisGame {
 	g := &tennisgame.TennisGame{}
-	assert.Equal(t, "Love All", g.Score(), "")
+	return g
 }
